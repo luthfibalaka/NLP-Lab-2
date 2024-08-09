c4afe14ead960fd8ee7299a1413fe2f52d48479a
luthfibalaka <luthfibalaka@gmail.com>
2024-08-09 10:51:36 +0700

    add sample input for hitung_akurasi

--- akurasi.py
+++ akurasi.py
@@ -5,4 +5,19 @@ def hitung_akurasi(tokenizer_tokens: list[list[str]], gold_std_tokens: list[list
     menerima hasil tokenisasi dari semua teks di dataset testing sekaligus)
     """
     # TODO: Hitung akurasi
-    pass
+    return -1
+
+if __name__ == "__main__":
+    # Contoh pemanggilan akurasi seperti pada dokumen soal
+    tokenizer_tokens = [
+        ["Bukunya", "mahal", "."],
+        ["Seharus", "nya", "kamu", "tidak", "terlambat", "."],
+    ]
+
+    gold_std_tokens = [
+        ["Buku", "nya", "mahal", "."],
+        ["Seharusnya", "kamu", "tidak", "terlambat", "."],
+    ]
+
+    epsilon = 1e-7
+    assert abs(hitung_akurasi(tokenizer_tokens, gold_std_tokens) - 0.67) <= epsilon
