b21519d6058c543dc91bc56225bd7a88325d0eb5
luthfibalaka <luthfibalaka@gmail.com>
2024-08-09 10:44:39 +0700

    improve unclear description

--- akurasi.py
+++ akurasi.py
@@ -1,7 +1,8 @@
 def hitung_akurasi(tokenizer_tokens: list[list[str]], gold_std_tokens: list[list[str]]):
     """
     Fungsi ini menghitung akurasi dari hasil tokenisasi suatu tokenizer
-    terhadap hasil tokenisasi gold standard
+    terhadap hasil tokenisasi gold standard (asumsikan bahwa fungsi ini
+    menerima hasil tokenisasi dari semua teks di dataset testing sekaligus)
     """
     # TODO: Hitung akurasi
     pass
